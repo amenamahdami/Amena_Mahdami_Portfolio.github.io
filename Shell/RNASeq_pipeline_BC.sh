0b5d64ad04d2b9026fce85310bd5cedb9f98748e
amenamahdami <117296183+amenamahdami@users.noreply.github.com>
2024-02-03 21:24:57 +0100

    Update RNASeq_pipeline_BC.sh

--- Shell/RNASeq_pipeline_BC.sh
+++ Shell/RNASeq_pipeline_BC.sh
@@ -7,10 +7,10 @@ cd /home/u0149935/RNASeq_pipeline/
 
 
 # STEP 1: Run fastqc
-#fastqc data/control.fastq -o data/
+fastqc data/control.fastq -o data/
 fastqc data/control2.fastq -o data/
 fastqc data/control3.fastq -o data/
-#fastqc data/silenced.fastq -o data/
+fastqc data/silenced.fastq -o data/
 fastqc data/silenced2.fastq -o data/
 fastqc data/silenced3.fastq -o data/
 
@@ -21,10 +21,10 @@ fastqc data/silenced3.fastq -o data/
 # wget https://genome-idx.s3.amazonaws.com/hisat/grch38_genome.tar.gz
 
 # run alignment
-#hisat2 -q --rna-strandness R -x genome/grch38/genome -U data/control.fastq | samtools sort -o data/control.bam
+hisat2 -q --rna-strandness R -x genome/grch38/genome -U data/control.fastq | samtools sort -o data/control.bam
 hisat2 -q --rna-strandness R -x genome/grch38/genome -U data/control2.fastq | samtools sort -o data/control2.bam
 hisat2 -q --rna-strandness R -x genome/grch38/genome -U data/control3.fastq | samtools sort -o data/control3.bam
-#hisat2 -q --rna-strandness R -x genome/grch38/genome -U data/silenced.fastq | samtools sort -o data/silenced.bam
+hisat2 -q --rna-strandness R -x genome/grch38/genome -U data/silenced.fastq | samtools sort -o data/silenced.bam
 hisat2 -q --rna-strandness R -x genome/grch38/genome -U data/silenced2.fastq | samtools sort -o data/silenced2.bam
 hisat2 -q --rna-strandness R -x genome/grch38/genome -U data/silenced3.fastq | samtools sort -o data/silenced3.bam
 echo "HISAT2 finished running! ^.^"
